083b7af6d9edefec1197b3f9b5052d7406031641
MsekniFiras <msekni.firas@gmail.com>
2023-08-02 13:30:29 -0700

    c

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-import sys
+"""Module that lists all states from the database"""
+
 import MySQLdb
+from sys import argv
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY states.id ASC")
-    [print(state) for state in c.fetchall()]
+    connection = MySQLdb.connect(host="localhost", port=3306,
+                                 user=argv[1], passwd=argv[2], db=argv[3])
+    """Connecting to the MySQL server"""
+
+    cursor = connection.cursor()
+    """Creating a cursor object to execute queries"""
+
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    """Executing the SELECT query to retrieve all states"""
+
+    lines = cursor.fetchall()
+    """Fetching all the rows returned by the query"""
+
+    for line in lines:
+        """Display the results"""
+        print(line)
+
+    cursor.close()
+    connection.close()
+    """Closing the cursor and the connection"""
